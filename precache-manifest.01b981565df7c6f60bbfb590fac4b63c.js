5c13ac4342a5cdeac8692124cd87563081aaa485
Andre Felipe <afelipes@gmail.com>
2022-09-24 20:29:54 -0300

    Updates

---
+++ precache-manifest.01b981565df7c6f60bbfb590fac4b63c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9327020d4594bba63f002e2bc1fdbfac",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "0acbaed679b8c9ce52f4",
+    "url": "/online-store/static/css/main.f7a36ed1.chunk.css"
+  },
+  {
+    "revision": "48c230c5e55825ad1a73",
+    "url": "/online-store/static/js/2.3fe620aa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/online-store/static/js/2.3fe620aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0acbaed679b8c9ce52f4",
+    "url": "/online-store/static/js/main.41cf2922.chunk.js"
+  },
+  {
+    "revision": "ff4896c2419cfc1775c2",
+    "url": "/online-store/static/js/runtime-main.a451c0e5.js"
+  },
+  {
+    "revision": "49a8aa6a57c22bdd565d5c0597c3ea41",
+    "url": "/online-store/static/media/cart-shopping-solid.49a8aa6a.svg"
+  },
+  {
+    "revision": "484d8b333862fa5182cf3b5248b79496",
+    "url": "/online-store/static/media/magnifying-glass-solid.484d8b33.svg"
+  },
+  {
+    "revision": "b9356c1bc4521251f368c4bcee187891",
+    "url": "/online-store/static/media/star-regular.b9356c1b.svg"
+  },
+  {
+    "revision": "c2366c0380b3c464dc24a5c070dc882a",
+    "url": "/online-store/static/media/star-solid.c2366c03.svg"
+  }
+]);
